259ce68ccdd5f4752a26d0c15407ffc6ccd53499
sibond <sibond3@gmail.com>
2023-07-23 16:02:18 +0100

    add component list

--- reddit-slim/src/App.js
+++ reddit-slim/src/App.js
@@ -1,18 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import List from "./components/List"
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
         <p>
           Reddit-Slim
         </p>
-       
-         
       </header>
+      <main>
+        <List />
+      </main>
     </div>
   );
 }
