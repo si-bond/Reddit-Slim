f51763e08b0741492c0866265bd7eba9b65bb458
sb-portfolio <sibond3@gmail.com>
2023-07-28 13:47:46 +0100

    fetch data

--- reddit-slim/src/App.js
+++ reddit-slim/src/App.js
@@ -1,9 +1,18 @@
+import React, {useState, useEffect} from 'react'
 import logo from './logo.svg';
 import './App.css';
 import List from "./components/List"
 
 
 function App() {
+
+  const [data, setData] = useState("")
+
+
+  useEffect(() => {
+    fetch("url")    
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
