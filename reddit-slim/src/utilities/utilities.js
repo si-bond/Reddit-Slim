70222685c9774bed2c2221f81e98e2bec892325e
sb-portfolio <sibond3@gmail.com>
2023-08-07 15:26:45 +0100

    format comment time in hours

--- reddit-slim/src/utilities/utilities.js
+++ reddit-slim/src/utilities/utilities.js
@@ -5,7 +5,11 @@ function timeAgoCalc(timeStamp){
     const timeDifference = currentdateTime - timeStamp
     const timeDifferenceHours = (timeDifference / (60 * 60)).toFixed(0)
 
-    if(timeDifferenceHours<24)
+    if(timeDifferenceHours==1)
+    {
+        return `${timeDifferenceHours} hour ago`
+    } 
+    else if (timeDifferenceHours<24)
     {
         return `${timeDifferenceHours} hours ago`
     } 
